a1eb525930e9141137abed3b952602112597aded
hypend12 <hypend12@gmail.com>
2022-10-28 22:25:56 -0400

    youtube-cone

---
+++ src/utills/fetchFromAPI.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '0b5b0d0c30mshc294ce9878be7e9p1c6812jsn4a770dfb6e0a',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
